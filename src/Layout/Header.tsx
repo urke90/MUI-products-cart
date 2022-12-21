3be62e58779789df8fb9b55bbaabb88fac3c31e1
urke90 <urosbijelic90@gmail.com>
2022-12-21 16:23:56 +0100

    created Header component and NavMenu.tsx component that represents navigation for Header (make it reusable if needed)

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,12 +1,31 @@
-import { Box } from '@mui/material';
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import LocalGroceryStoreRoundedIcon from '@mui/icons-material/LocalGroceryStoreRounded';
+
+import NavMenu from '../common/navigation/NavMenu';
 
 interface IHeaderProps {}
 
 const Header: React.FC<IHeaderProps> = () => {
     return (
-        <Box sx={{ backgroundColor: 'primary.main', py: 3 }}>
-            <Container maxWidth="lg">THIS IS HEADER</Container>
+        <Box sx={{ backgroundColor: 'primary.main', py: 1 }}>
+            <Container maxWidth="lg">
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center'
+                    }}
+                >
+                    <LocalGroceryStoreRoundedIcon
+                        sx={{
+                            color: 'secondary.light'
+                        }}
+                        fontSize="large"
+                    />
+                    <NavMenu />
+                </Box>
+            </Container>
         </Box>
     );
 };
