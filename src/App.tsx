72378b4ac4ba20d91cc2b211a98a7a29b3d450c9
urke90 <urosbijelic90@gmail.com>
2022-12-23 15:19:32 +0100

    created loading spinner component which can be used on some item or as overlay moved logic for fething data to BooksGrid.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,16 @@
-import { useEffect, useState } from 'react';
-
-import { Box } from '@mui/system';
+import Box from '@mui/system/Box';
 import Container from '@mui/material/Container';
-import { useAxios } from './hooks/use-axios';
-import { IBook } from './ts/books';
 
 import Header from './Layout/Header';
 import BooksGrid from './components/books/BooksGrid';
 
 const App: React.FC = () => {
-    const [books, setBooks] = useState<IBook[]>([]);
-    const { sendRequest } = useAxios();
-
-    useEffect(() => {
-        const fetchBooks = async () => {
-            const response = await sendRequest({
-                url: 'books.json',
-                method: 'GET'
-            });
-
-            if (response?.status === 200 && response.data.length > 0) {
-                setBooks(response.data);
-            }
-
-            console.log('response', response);
-        };
-        fetchBooks();
-    }, [sendRequest]);
-
-    console.log('books fetched', books);
-
     return (
         <Box>
             <Header />
             <Container maxWidth="lg">
                 <Box>
-                    {books.length > 0 ? <BooksGrid books={books} /> : null}
+                    <BooksGrid />
                 </Box>
             </Container>
         </Box>
