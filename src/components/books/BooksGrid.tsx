727d59f5e63fd8e1a78d13c5b56291a116ac2eee
urke90 <urosbijelic90@gmail.com>
2022-12-24 19:31:07 +0100

    updated useAxios hook to fetch data inside hook and then return it with generic type of data

--- src/components/books/BooksGrid.tsx
+++ src/components/books/BooksGrid.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -7,44 +7,105 @@ import { useAxios } from '../../hooks/use-axios';
 import { IBook } from '../../ts/books';
 
 import BooksGridItem from './BooksGridItem';
+import LoadingSpinner from '../../common/ui/LoadingSpinner';
 
 interface IBooksGridProps {}
 
+/**
+ *
+ * * LEFT COMMENTED CODE TO BE REVIEWED
+ */
+
 const BooksGrid: React.FC<IBooksGridProps> = () => {
-    const [books, setBooks] = useState<IBook[]>([]);
-    const { sendRequest } = useAxios();
+    // const [books, setBooks] = useState<IBook[]>([]);
+    const {
+        data: books,
+        isLoading,
+        error
+    } = useAxios<IBook[]>({ url: 'books.json', method: 'GET' }, true);
 
-    useEffect(() => {
-        const fetchBooks = async () => {
-            const response = await sendRequest({
-                url: 'books.json',
-                method: 'GET'
-            });
+    // console.log('books', books);
 
-            if (response?.status === 200 && response.data.length > 0) {
-                setBooks(response.data);
-            }
+    // useMemo(() => {
+    //     console.log('opalio useMemo');
+    //     const fetchBooks = async () => {
+    //         const response = await sendRequest({
+    //             url: 'books.json',
+    //             method: 'GET'
+    //         });
 
-            console.log('response', response);
-        };
-        fetchBooks();
-    }, [sendRequest]);
+    //         if (response?.status === 200 && response.data.length > 0) {
+    //             const modifiedBooks: IBook[] = response.data.reduce(
+    //                 (acc: IBook[], nextBook: IBook, index: number) => {
+    //                     const updatedBook = {
+    //                         ...nextBook,
+    //                         id: index + 1
+    //                     };
 
-    console.log('books fetched', books);
+    //                     return [...acc, updatedBook];
+    //                 },
+    //                 []
+    //             );
+
+    //             setBooks(modifiedBooks);
+    //         }
+    //     };
+    //     fetchBooks();
+    // }, []);
+
+    if (isLoading) {
+        return <LoadingSpinner asOverlay />;
+    } else if (error) {
+        return (
+            <Box sx={{ py: 3 }}>
+                <Typography variant="h2" align="center" gutterBottom>
+                    Something went wrong, could not fetch books
+                </Typography>
+            </Box>
+        );
+    } else if (!isLoading && !error && books?.length === 0) {
+        return (
+            <Typography variant="h2" align="center" gutterBottom>
+                There are no books to display
+            </Typography>
+        );
+    }
+
     return (
         <Box sx={{ py: 3 }}>
             <Typography variant="h2" align="center" gutterBottom>
                 Books
             </Typography>
             <Grid container spacing={3}>
-                <BooksGridItem />
-                <BooksGridItem />
-                <BooksGridItem />
-                <BooksGridItem />
-                <BooksGridItem />
-                <BooksGridItem />
+                {books && books.length > 0
+                    ? books.map((book) => (
+                          <BooksGridItem
+                              key={book.link + book.imageLink}
+                              book={book}
+                          />
+                      ))
+                    : null}
             </Grid>
         </Box>
     );
 };
 export default BooksGrid;
+
+/**
+ * const Component = () => {
+   useMemo(() => {
+     // componentWillMount events
+   },[]);
+   useEffect(() => {
+     // componentDidMount events
+     return () => {
+       // componentWillUnmount events
+     }
+   }, []);
+};
+
+
+You would need to keep the useMemo hook before anything that interacts with your state. This is not how it is intended but it worked for me for all componentWillMount issues.
+
+This works because useMemo doesnt require to actually return a value and you dont have to actually use it as anything, but since it memorizes a value based on dependencies which will only run once ("[]") and its on top of our component it runs once when the component mounts before anything else.
+ */
